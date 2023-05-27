cce520c96a60ec9a37c9911ee640df50d1ef9797
aldrinbalitbit <aldrinbalitbit1127@gmail.com>
2023-05-27 14:40:58 +0800

    Use on back dir

--- build.ps1
+++ build.ps1
@@ -12,6 +12,7 @@ _msg "Entering stepmania"
 git clone https://github.com/stepmania/stepmania.git
 cd stepmania/Build
 cmake -DCMAKE_BUILD_TYPE=Release -G "Visual Studio 17 2022" -A x64 ..
+ls
+msbuild StepMania.sln /p:Platform="x64" /p:Configuration="Release"
 ls ..
 cd ..
-msbuild StepMania.sln /p:Platform="x64" /p:Configuration="Release"
