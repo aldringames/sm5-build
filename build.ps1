98512a3d6331310df866c40754da0e2b8ef1679d
aldrinbalitbit <aldrinbalitbit1127@gmail.com>
2023-05-28 07:49:52 +0800

    Using a recent themes from SM-SSC 1.2.5

--- build.ps1
+++ build.ps1
@@ -9,13 +9,27 @@ function _msg {
 }
 
 _msg "Entering stepmania"
-git clone https://github.com/stepmania/stepmania.git
+git clone --depth=1 https://github.com/stepmania/stepmania.git
 cd stepmania/Build
 _msg "Configuring stepmania"
 cmake -DCMAKE_BUILD_TYPE=Release -G "Visual Studio 17 2022" -A x64 ..
 _msg "Building stepmania"
 msbuild StepMania.sln /p:Platform="x64" /p:Configuration="Release"
 cd ..
+_msg "Using SM5-Build derivatives"
+git clone --depth=1 -b v1.2.5 https://github.com/stepmania/stepmania.git stepmania-legacy
+Remove-Item src/archutils/Win32/StepMania.ico
+Copy-Item stepmania-legacy/src/archutils/Win32/StepMania.ico src/archutils/Win32
+Remove-Item Data/splash.png
+Copy-Item stepmania-legacy/Data/splash.png Data
+Remove-Item -Recurse stepmania-legacy
+Remove-Item -Recurse Themes/default
+Remove-Item -Recurse Themes/home
+Remove-Item -Recurse Themes/legacy
+git clone --depth=1 https://github.com/Simply-Love/Simply-Love-SM5 "Themes/Simply Love"
+git clone --depth=1 https://github.com/JoseVarelaP/SM5-GrooveNights "Themes/ITG GrooveNights"
+git clone --depth=1 https://github.com/MidflightDigital/XX--STARLiGHT--twopointzero "Themes/DDR XX -STARLiGHT- 2.0"
+Remove-Item -Recurse Themes/*/.git
 _msg "Copying StepMania files"
 mkdir -p Dist/StepMania
 Copy-Item -Recurse -Force -Path Announcers -Destination Dist/StepMania
@@ -37,5 +51,4 @@ $datestamp | Out-File -FilePath Dist/StepMania/datestamp
 "" | Out-File -FilePath Dist/StepMania/portable.ini
 _msg "Create archive as a SM5-Build"
 7z a SM5-Build-win64-$datestamp.zip Dist/StepMania
-ls
 ls Dist/StepMania
