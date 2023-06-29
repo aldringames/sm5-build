e79fc37ca7c15bda7b88451a7aa0b0f5c5d63407
aldrinbalitbit <aldrinbalitbit1127@gmail.com>
2023-06-29 12:07:00 +0800

    Fix Xcode Generator

--- build-mac.sh
+++ build-mac.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 _msg() {
-	echo -e "\e[1;32m>> \e[m$1..."
+	echo -e "\e[1;32m>>\e[m $1"
 }
 
 for arch in x86_64 arm64
@@ -13,7 +13,7 @@ do
 	git clone --depth=1 https://github.com/Simply-Love/Simply-Love-SM5.git "Themes/Simply Love"
 	rm -rf Themes/*/.git
 	_msg "Configuring sm (${arch})"
-	cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_OSX_ARCHITECTURES="${arch}" -G XCode ..
+	cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_OSX_ARCHITECTURES="${arch}" -G Xcode ..
 	_msg "Building sm"
 	ls
 	xcodebuild -project StepMania.xcodeproj -configuration Release
