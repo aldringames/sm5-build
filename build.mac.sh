ed89ce030c925e945f5c06d21c560a9288de9176
aldrinbalitbit <aldrinbalitbit1127@gmail.com>
2023-06-30 19:34:45 +0800

    Refund commit on oses

---
+++ build.mac.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -e
+_msg() {
+	echo -e "\e[1;32m>>\e[m $1"
+}
+
+mkdir StepMania
+_msg "Entering sm"
+git clone --depth=1 --recurse-submodules https://github.com/stepmania/stepmania.git sm
+cd sm/Build
+_msg "Configuring sm"
+cmake -DCMAKE_BUILD_TYPE=Release >> ../../StepMania/build.log
+_msg "Building sm"
+cmake --build . >> ../../StepMania/build.log
+cd ..
+ls
